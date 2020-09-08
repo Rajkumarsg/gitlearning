da15c5be147efe1728fbae83dc5ea37076489b83
Rajkumarsg <rajuusg.dvg@gmail.com>
2020-09-08 15:48:17 +0530

    function added

--- module1.py
+++ module1.py
@@ -11,7 +11,10 @@ app = Flask(__name__)
 # and add decorators to define the appropriate resource locators for them.
 
 @app.route('/')
-@app.route('/hello')
+@app.route('/hi')
+def hi():
+    # Render the page
+    return "Hello Python! "
 def hello():
     # Render the page
     return "Hello Python! by raj"
